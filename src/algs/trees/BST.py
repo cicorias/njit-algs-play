83e97a6f80a10298244c284046582b16ddc23cd3
Shawn Cicoria <github@cicoria.com>
2020-08-04 16:06:09 -0400

    bst to array

--- src/algs/trees/BST.py
+++ src/algs/trees/BST.py
@@ -2,20 +2,92 @@ from __future__ import annotations
 
 
 class BSTNode:
-    def __init__(self, key: int, left: BSTNode=None, right: BSTNode= None) -> None:
-        self.key = key
-        self.right = right
-        self.left = left
+    def __init__(self, key: int, left: BSTNode = None, right: BSTNode = None) -> None:
+        self._key = key
+        self._right = right
+        self._left = left
+
+    @property
+    def left(self):
+        return self._left
+
+    @left.setter
+    def left(self, n: BSTNode):
+        self._left = n
+
+    @property
+    def right(self):
+        return self._right
+
+    @right.setter
+    def right(self, n: BSTNode):
+        self._right = n
+
+    @property
+    def key(self):
+        return self._key
+
+    @key.setter
+    def key(self, v: int):
+        self.key = v
+
+
+class BST:
+    def __init__(self, root: BSTNode = None) -> None:
+        self.root = root
 
     def print_tree(self):
         pass
 
+    def makeList(self, T: BSTNode, a=[]):
+        if T != None:
+            self.makeList(T.left, a)
+            a += [T.key]
+            self.makeList(T.right, a)
+        return a
 
-    def get_array(self):
-        pass
+    def makeList2(self, T: BSTNode):
+        if T is None:
+            return []
+        return self.makeList2(T.left) + [T.key] + self.makeList2(T.right)
 
+    def get_array(self, T: BSTNode, a = []):
+        if T is None:
+            return
+        
+        self.get_array(T.left, a)
+        a += [T.key]
+        self.get_array(T.right, a)
 
-class BST:
-    def __init__(self, root: BSTNode=None) -> None:
-        self.root = root
+        return a
 
+    # Function to  print level order traversal of tree
+    def printLevelOrder(self, root):
+        h = self.height(root)
+        for i in range(1, h+1):
+            self.printGivenLevel(root, i)
+
+    # Print nodes at a given level
+
+    def printGivenLevel(self, root, level):
+        if root is None:
+            return
+        if level == 1:
+            print("{}".format(root.key)),
+        elif level > 1:
+            self.printGivenLevel(root.left, level-1)
+            self.printGivenLevel(root.right, level-1)
+
+    def height(self, node):
+        if node is None:
+            return 0
+        else:
+            # Compute the height of each subtree
+            lheight = self.height(node.left)
+            rheight = self.height(node.right)
+
+            # Use the larger one
+            if lheight > rheight:
+                return lheight+1
+            else:
+                return rheight+1
--- src/tests/bst_test.py
+++ src/tests/bst_test.py
@@ -1,19 +1,45 @@
 import unittest
 
-from src.algs.trees.BST import BSTNode
+from src.algs.trees.BST import BST, BSTNode
 
 class Test_Bst(unittest.TestCase):
     def setUp(self) -> None:
         self.root = BSTNode(4)
 
+# '''
+#         4
+#        / \
+#       /   \
+#      2     6
+#     / \   / \
+#    1   3 5   7
 
-    def make_tree(self):
+# '''
+    def make_tree(self) -> BSTNode:
         root = BSTNode(4)
-        
+        root.left = BSTNode(2)
+        root.right = BSTNode(6)
+        root.left.left = BSTNode(1)
+        root.left.right = BSTNode(3)
+        root.right.left = BSTNode(5)
+        root.right.right = BSTNode(7)
 
-    def test_one(self):
-        vi = 1 + 1
-        self.assertEqual(vi, 2)
+        return root
+
+    def test_get_array(self):
+        node = self.make_tree()
+        bst = BST()
+        exp = [1, 2, 3, 4, 5, 6, 7]
+        act = bst.get_array(node)
+        act2 = bst.makeList(node)
+        act3 = bst.makeList2(node)
+        self.assertListEqual(exp, act, 'get array failed')
+
+    def test_bst_print(self):
+        node = self.make_tree()
+        bst = BST()
+        bst.printLevelOrder(node)
+        self.assertEqual(bst.height, 3)
 
 
 if __name__ == "__main__":
